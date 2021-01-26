596524b171d95af2e36f306847ee15cbad87d1f3
mohi22 <mohi_migue2210@yahoo.com.ar>
2021-01-26 18:15:20 -0300

    se agrega archivo comercios.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends BaseController
 	public function index()
 	{
 		echo( view('header.php'));
-		echo( view('welcome_message') );
+		echo( view('comercios.php'));
 		echo( view('footer.php'));
 	}
 
